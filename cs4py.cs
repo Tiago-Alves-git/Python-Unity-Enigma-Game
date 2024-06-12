817fefcc65672912e433605a8df986dbe54c18f2
TIAGO ALVES FERNANDES <24101255@aluno.idp.edu.br>
2024-06-12 14:24:35 -0300

    Start Setup C# and Python Game

---
+++ cs4py.cs
@@ -0,0 +1,46 @@
+// Unity C# script
+using UnityEngine;
+using System.Diagnostics;
+using System.IO;
+using System;
+
+public class PythonIntegration : MonoBehaviour
+{
+    Process pythonProcess;
+    StreamWriter pythonStreamWriter;
+    StreamReader pythonStreamReader;
+
+    void Start()
+    {
+        StartPythonProcess();
+    }
+
+    void StartPythonProcess()
+    {
+        pythonProcess = new Process();
+        pythonProcess.StartInfo.FileName = "python";
+        pythonProcess.StartInfo.Arguments = "path_to_python_script.py";
+        pythonProcess.StartInfo.UseShellExecute = false;
+        pythonProcess.StartInfo.RedirectStandardOutput = true;
+        pythonProcess.StartInfo.RedirectStandardInput = true;
+        pythonProcess.Start();
+        
+        pythonStreamWriter = pythonProcess.StandardInput;
+        pythonStreamReader = pythonProcess.StandardOutput;
+    }
+
+    void SendToPython(string message)
+    {
+        pythonStreamWriter.WriteLine(message);
+        string response = pythonStreamReader.ReadLine();
+        UnityEngine.Debug.Log("Python response: " + response);
+    }
+
+    void OnApplicationQuit()
+    {
+        if (pythonProcess != null && !pythonProcess.HasExited)
+        {
+            pythonProcess.Kill();
+        }
+    }
+}
